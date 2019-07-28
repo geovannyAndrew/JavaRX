5c2c68e123856922f19bc9e82b838ee1e3aedb83
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-07-27 21:05:08 -0500

    flatMap Operator

--- app/src/main/java/com/gyros/rxjava/MainActivity.java
+++ app/src/main/java/com/gyros/rxjava/MainActivity.java
@@ -1,18 +1,30 @@
 package com.gyros.rxjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.gyros.rxjava.models.Comment;
+import com.gyros.rxjava.models.Post;
+import com.gyros.rxjava.requests.ServiceGenerator;
+
+import java.util.List;
+import java.util.Random;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
+import retrofit2.http.POST;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
 
     private CompositeDisposable disposables = new CompositeDisposable();
+    private RecyclerView recyclerView;
+    private RecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +42,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+        recyclerView = findViewById(R.id.recycler_view);
+        //testSimpleSubcribe();
+        initRecyclerView();
+        getPostsObservable()
+                .subscribeOn(Schedulers.io())
+                .flatMap(new Function<Post, ObservableSource<Post>>() {
+                    @Override
+                    public ObservableSource<Post> apply(Post post) throws Exception {
+                        //Thread.sleep(200);
+                        return getCommentsObservable(post);
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Post>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposables.add(d);
+                    }
 
+                    @Override
+                    public void onNext(Post post) {
+                        //Log.d(TAG,"onNext"+post.getBody());
+                        adapter.updatePost(post);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG,"onError:",e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
+    private void testSimpleSubcribe(){
         Observable<Task> taskObservable = Observable
                 .fromIterable(DataSource.Companion.createTaskList())
                 .subscribeOn(Schedulers.io())
@@ -69,6 +121,45 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG,"onComplete: called");
             }
         });
+    }
+
+    private void initRecyclerView(){
+        adapter = new RecyclerAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private Observable<Post> getCommentsObservable(final Post post){
+        return ServiceGenerator.getRequestApi()
+                .getComments(post.getId())
+                .map(new Function<List<Comment>, Post>() {
+                    @Override
+                    public Post apply(List<Comment> comments) throws Exception {
+                        int delay = ((new Random()).nextInt(5) + 1) * 1000; // sleep thread for x ms
+                        Thread.sleep(delay);
+                        Log.d(TAG, "apply: sleeping thread " + Thread.currentThread().getName() + " for " + String.valueOf(delay)+ "ms");
+
+                        post.setComments(comments);
+                         return post;
+                    }
+                }).subscribeOn(Schedulers.io());
+    }
+
+
+    private Observable<Post> getPostsObservable(){
+        return ServiceGenerator.getRequestApi()
+                .getPosts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Function<List<Post>, ObservableSource<Post>>() {
+                    @Override
+                    public ObservableSource<Post> apply(List<Post> posts) throws Exception {
+                        adapter.setPosts(posts);
+                        return Observable.fromIterable(posts)
+                                .subscribeOn(Schedulers.io());
+
+                    }
+                });
     }
 
 
